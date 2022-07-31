0a802352b999589c1c3e40b492818c207697fb7e
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-07-31 17:38:36 +0300

    almost finished

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,8 +1,52 @@
-
+import { useEffect, useState } from "react";
 import PokemonList from "../pokemonList/PokemonList";
+import axios from "axios";
 
 const PokemonCard = () => {
-  return <></>;
+  const [pokemon, setPokemon] = useState("");
+  const [pokemonName, setPokemonName] = useState("");
+
+  useEffect(() => {
+    async function getPokemonApi() {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154`
+        );
+        console.log(response);
+        setPokemon(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPokemonApi();
+  }, [pokemonName]);
+
+  const handleChange = (e) => {
+    setPokemonName(e.target.value);
+  };
+
+  return (
+    <div className="page">
+      <div>
+        <div className="header">
+          {" "}
+          <label htmlFor="header">PoKéMoN</label>
+        </div>
+        <input type="text" id="header" onChange={handleChange} />
+      </div>
+      <div className="container-card">
+        {!pokemonName
+          ? pokemon.map((pokes) => {
+              return <PokemonList {...pokes} />;
+            })
+          : pokemon.map((pokes) => {
+              return (
+                pokes.name.includes(pokemonName) && <PokemonList {...pokes} />
+              );
+            })}
+      </div>
+    </div>
+  );
 };
 
 export default PokemonCard;
--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -1,7 +1,15 @@
 import "./PokemonList.css";
 
-const PokemonList = () => {
-  return <></>;
+const PokemonList = ({ name, url, index }) => {
+  const imgIndex = url.split("/")[url.split("/").length - 2];
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imgIndex}.png`;
+  console.log(index);
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+      <img src={imgUrl} alt="" />
+    </div>
+  );
 };
 
 export default PokemonList;
